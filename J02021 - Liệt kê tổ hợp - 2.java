1bbac9f3e9ff08559661c8cbc3777823bab90aff
Duy Anh Nguyen <139189890+duyanh221204@users.noreply.github.com>
2024-08-21 22:44:21 +0700

    Create J02021 - Liệt kê tổ hợp - 2.java

---
+++ J02021 - Liệt kê tổ hợp - 2.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(), k = sc.nextInt(), d = 0;
+        int[] a = new int[k + 2];
+        for (int i = 1; i <= k; ++i) a[i] = i;
+        while (true)
+        {
+            ++d;
+            for (int i = 1; i <= k; ++i) System.out.print (a[i]);
+            System.out.print (" ");
+            int x = k;
+            while (x >= 1 && a[x] == n - k + x) --x;
+            if (x == 0)
+            {
+                System.out.println ();
+                break;
+            }
+            ++a[x];
+            for (int i = x + 1; i <= k; ++i) a[i] = a[i - 1] + 1;
+        }
+        System.out.print ("Tong cong co " + d + " to hop");
+    }
+}
