78c1cc32fe24f1dd30cb2bad09c4e083862d5d3d
Duy Anh Nguyen <139189890+duyanh221204@users.noreply.github.com>
2024-09-18 23:32:03 +0700

    Create J07017 - Lớp Pair (generic).java

--- J07017 - Lớp Pair (generic).java
+++ J07017 - Lớp Pair (generic).java
@@ -49,38 +49,4 @@ public class Main
             if(!check) System.out.println(-1);
         }
     }
-    public static void main47711(String[] args) throws IOException {
-        Scanner sc = new Scanner(new File("DATA.in"));
-        int t = sc.nextInt();
-        while(t-->0){
-            int n = sc.nextInt();
-            boolean check = false;
-            for(int i = 2; i <= 2*Math.sqrt(n); i++){
-                Pair<Integer, Integer> p = new Pair<>(i, n-i);
-                if(p.isPrime()){
-                    System.out.println(p);
-                    check = true;
-                    break; 
-                }
-            }
-            if(!check) System.out.println(-1);
-        }
-    }
-    public static void main1048922(String[] args) throws IOException {
-        Scanner sc = new Scanner(new File("DATA.in"));
-        int t = sc.nextInt();
-        while(t-->0){
-            int n = sc.nextInt();
-            boolean check = false;
-            for(int i = 2; i <= 2*Math.sqrt(n); i++){
-                Pair<Integer, Integer> p = new Pair<>(i, n-i);
-                if(p.isPrime()){
-                    System.out.println(p);
-                    check = true;
-                    break;
-                }
-            }
-            if(!check) System.out.println(-1);
-        }
-    }
 }
