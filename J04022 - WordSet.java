b3f44997d1d1cac38ed0e237b6bea1c1689452ed
Duy Anh Nguyen <139189890+duyanh221204@users.noreply.github.com>
2024-09-19 10:28:56 +0700

    Create J04022 - WordSet.java

--- J04022 - WordSet.java
+++ J04022 - WordSet.java
@@ -44,11 +44,4 @@ public class Main
         System.out.println(s1.union(s2));
         System.out.println(s1.intersection(s2));
     }
-    public static void main7947670(String[] args) {
-        Scanner in = new Scanner(System.in);
-        WordSet s1 = new WordSet(in.nextLine());
-        WordSet s2 = new WordSet(in.nextLine());
-        System.out.println(s1.union(s2));
-        System.out.println(s1.intersection(s2));
-    }
 }
