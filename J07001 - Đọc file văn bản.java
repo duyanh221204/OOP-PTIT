1edaed90c58e7b0654115874ec3a6b6dbdb79859
Duy Anh Nguyen <139189890+duyanh221204@users.noreply.github.com>
2024-09-19 20:28:13 +0700

    Create J07001 - Đọc file văn bản.java

---
+++ J07001 - Đọc file văn bản.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.io.*;
+public class Main
+{
+    public static void main(String[] args) throws FileNotFoundException
+    {
+        Scanner sc = new Scanner(new File("DATA.in"));
+        while (sc.hasNextLine()) System.out.println (sc.nextLine());
+    }
+}
