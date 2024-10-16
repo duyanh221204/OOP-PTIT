8788e85f87af9ede02f2cca70360a04a6a8ca123
Duy Anh Nguyen <139189890+duyanh221204@users.noreply.github.com>
2024-10-16 22:05:54 +0700

    Create J04009 - Diện tích tam giác.java

---
+++ J04009 - Diện tích tam giác.java
@@ -0,0 +1,57 @@
+import java.util.*;
+class Point
+{
+    private double x, y;
+    public Point()
+    {
+    }
+    public void setX(double x)
+    {
+        this.x = x;
+    }
+    public void setY(double y)
+    {
+        this.y = y;
+    }
+    public double dist(Point p)
+    {
+        return Math.sqrt((this.x - p.x) * (this.x - p.x) + (this.y - p.y) * (this.y - p.y));
+    }
+}
+class Triangle
+{
+    private Point a = new Point(), b = new Point(), c = new Point();
+    private double d1, d2, d3;
+    public Triangle(double x1, double y1, double x2, double y2, double x3, double y3)
+    {
+        a.setX(x1);
+        a.setY(y1);
+        b.setX(x2);
+        b.setY(y2);
+        c.setX(x3);
+        c.setY(y3);
+        this.d1 = a.dist(b);
+        this.d2 = b.dist(c);
+        this.d3 = c.dist(a);
+    }
+
+    @Override
+    public String toString()
+    {
+        if (this.d1 + this.d2 > this.d3 && this.d1 + this.d3 > this.d2 && this.d2 + this.d3 > this.d1)
+        {
+            double p = (this.d1 + this.d2 + this.d3) / 2;
+            return String.format ("%.2f", Math.sqrt(p * (p - this.d1) * (p - this.d2) * (p - this.d3)));
+        }
+        return "INVALID";
+    }
+}
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) System.out.println (new Triangle(sc.nextDouble(), sc.nextDouble(), sc.nextDouble(), sc.nextDouble(), sc.nextDouble(), sc.nextDouble()));
+    }
+}
